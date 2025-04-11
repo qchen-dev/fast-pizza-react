16a43fcf1e9751354073c1325757f128662fb6a3
Nick Chen <qche.au@gmail.com>
2025-04-12 01:00:38 +1000

    feat(layout): update AppLayout with Loader

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,18 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 
 import Header from "./Header";
+import Loader from "./Loader";
 import CartOverview from "../features/cart/CartOverview";
+import { NAVIGATION_STATE } from "../utils/constants";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === NAVIGATION_STATE.LOADING;
+  console.log(navigation);
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
 
       <main>
