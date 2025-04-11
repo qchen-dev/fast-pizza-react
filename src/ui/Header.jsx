54d4b2396a902094b7b4c96846ee14b3d94e2b59
Nick Chen <qche.au@gmail.com>
2025-04-11 22:47:26 +1000

    feat(header): add Header UI component

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">Fast React Pizza Co.</Link>
+
+      <p>User name</p>
+    </header>
+  );
+}
+
+export default Header;
