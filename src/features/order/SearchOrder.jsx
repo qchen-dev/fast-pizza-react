9c1563a1fa3226a9bda0427e3f3b3ad9ad7f9bb5
Nick Chen <qche.au@gmail.com>
2025-04-12 00:54:49 +1000

    feat: add search order form component

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
+function SearchOrder() {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  function handleQueryChange(evt) {
+    setQuery(() => evt.target.value);
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    if (!query) return;
+
+    navigate(`/order/${query}`);
+    setQuery(() => "");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Search order number"
+        value={query}
+        onChange={handleQueryChange}
+        type="text"
+      />
+    </form>
+  );
+}
+
+export default SearchOrder;
