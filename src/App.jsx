96b41cb9ff1a17af50af0c5540dd15eec34ba6f8
Nick Chen <qche.au@gmail.com>
2025-04-14 16:19:27 +1000

    feat: update App component with react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,59 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
-import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
-import CreateOrder from "./features/order/CreateOrder";
-import Order from "./features/order/Order";
-import AppLayout from "./ui/AppLayout";
+import { createBrowserRouter, RouterProvider } from 'react-router';
+
+import Home from './ui/Home';
+import Error from './ui/Error';
+import Menu from './features/menu/Menu';
+import { loader as menuLoader } from './features/menu/loader';
+import Cart from './features/cart/Cart';
+import CreateOrder, {
+  action as createOrderAction,
+} from './features/order/action';
+import Order from './features/order/Order';
+import { loader as orderLoader } from './features/order/loader';
+import AppLayout from './ui/AppLayout';
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
+
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Home />,
       },
       {
-        path: "/menu",
+        path: '/menu',
         element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
       },
       {
-        path: "/cart",
+        path: '/cart',
         element: <Cart />,
       },
       {
-        path: "/order/new",
+        path: '/order/new',
         element: <CreateOrder />,
+        action: createOrderAction,
       },
       {
-        path: "/order/:orderId",
+        path: '/order/:orderId',
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <RouterProvider
+      router={router}
+      fallbackElement={<p>Loading...</p>} // This is your HydrateFallback
+    />
+  );
 }
 
 export default App;
