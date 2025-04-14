0fb6b7e57b665a1f3e6d0a9b7ce053914189f607
Nick Chen <qche.au@gmail.com>
2025-04-14 16:10:35 +1000

    feat(ui): add SearchOrder component

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useState } from 'react';
+import { useNavigate } from 'react-router';
 
 function SearchOrder() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
   const navigate = useNavigate();
 
   function handleQueryChange(evt) {
@@ -14,7 +14,7 @@ function SearchOrder() {
     if (!query) return;
 
     navigate(`/order/${query}`);
-    setQuery(() => "");
+    setQuery(() => '');
   }
 
   return (
