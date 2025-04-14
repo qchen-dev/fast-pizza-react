fb10c4d2d8907cc74a1fdb87193a702af0bb13b6
Nick Chen <qche.au@gmail.com>
2025-04-14 16:11:14 +1000

    feat(ui): add Header UI component

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,9 @@
-import { Link } from "react-router";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from 'react-router';
+import SearchOrder from '../features/order/SearchOrder';
 
 function Header() {
   return (
-    <header>
+    <header className="bg-yellow-400">
       <Link to="/">Fast React Pizza Co.</Link>
 
       <SearchOrder />
