2f35f70c3ddbfb4b96d93c3b677af6bc17d6d913
Nick Chen <qche.au@gmail.com>
2025-04-14 16:13:48 +1000

    feat(ui): add CartOverview component

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -1,9 +1,9 @@
-import { Link } from "react-router";
+import { Link } from 'react-router';
 
 function CartOverview() {
   return (
-    <div>
-      <p>
+    <div className="bg-stone-700 text-stone-100">
+      <p className="text-stone-200">
         <span>23 pizzas</span>
         <span>$23.45</span>
       </p>
