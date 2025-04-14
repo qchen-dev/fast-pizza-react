f216f2a54c9f750d44d89cb8f4ba5dc16af618e5
Nick Chen <qche.au@gmail.com>
2025-04-14 16:17:11 +1000

    feat(ui): add Create Order UI component

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,31 +1,24 @@
-import { Form, redirect, useActionData, useNavigation } from "react-router";
-import { createOrder } from "../../services/apiRestaurant";
-import { NAVIGATION_STATE } from "../../utils/constants";
-
-// https://uibakery.io/regex-library/phone-number
-const isValidPhone = (str) =>
-  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
-  );
+import { Form, useActionData, useNavigation } from 'react-router';
+import { NAVIGATION_STATE } from '../../utils/constants';
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: "Mediterranean",
+    name: 'Mediterranean',
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: "Vegetale",
+    name: 'Vegetale',
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: "Spinach and Mushroom",
+    name: 'Spinach and Mushroom',
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -58,7 +51,7 @@ function CreateOrder() {
           <div>
             <input type="tel" name="phone" required />
           </div>
-          <p style={{ color: "red" }}>{errors?.phone && errors.phone}</p>
+          <p style={{ color: 'red' }}>{errors?.phone}</p>
         </div>
 
         <div>
@@ -82,39 +75,12 @@ function CreateOrder() {
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <button disabled={isSubmitting}>
-            {isSubmitting ? "Placing Order..." : "Order Now"}
+            {isSubmitting ? 'Placing Order...' : 'Order Now'}
           </button>
         </div>
       </Form>
     </div>
   );
-}
-
-export async function action({ request }) {
-  const formData = await request.formData();
-  const data = Object.fromEntries(formData);
-  // console.log(data);
-
-  const order = {
-    ...data,
-    cart: JSON.parse(data.cart),
-    priority: "on" === data.priority,
-  };
-
-  const errors = {};
-  if (!isValidPhone(order.phone)) {
-    errors.phone = "invalue phone number";
-  }
-
-  if (Object.keys(errors).length > 0) {
-    return errors;
-  }
-
-  // if everything is ok, create new order and redirect
-  const newOrder = await createOrder(order);
-  console.log(newOrder);
-
-  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
